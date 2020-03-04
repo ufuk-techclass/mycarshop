e7bf6991ab93902547940861594836f0cd93da89
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-03-04 19:34:40 +0200

    Updates

---
+++ precache-manifest.1f5b7c38df13b37f5c9f1f998831ef42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84da48519672cac1c20dd60e435419dd",
+    "url": "/mycarshop/index.html"
+  },
+  {
+    "revision": "9bd405aea9375bf5c597",
+    "url": "/mycarshop/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "1845b6b84522638174f5",
+    "url": "/mycarshop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bd405aea9375bf5c597",
+    "url": "/mycarshop/static/js/2.9a375442.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/mycarshop/static/js/2.9a375442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1845b6b84522638174f5",
+    "url": "/mycarshop/static/js/main.3633286b.chunk.js"
+  },
+  {
+    "revision": "965dab7d022431f3253f",
+    "url": "/mycarshop/static/js/runtime-main.cf966399.js"
+  }
+]);
